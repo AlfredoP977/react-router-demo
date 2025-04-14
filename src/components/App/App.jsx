65ad1a38400fa3c0544c19f91b289a35807e69dc
AlfredoP977 <A_parodi97@hotmail.com>
2025-04-13 21:51:33 -0400

    saving

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
 import Header from "../Header/Header";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "../Dashboard/Dashboard"; // New import
 
 function App() {
   return (
     <div className="App">
       <Header />
+      {/* Wrap a Route component inside a Routes component
+          and specify the path and element attributes as shown. */}
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
