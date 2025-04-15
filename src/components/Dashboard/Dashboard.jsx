b2ee8e339c8c3a3d28dc7211fc2ea386385863bc
AlfredoP977 <A_parodi97@hotmail.com>
2025-04-15 12:38:54 -0400

    added folder

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom";
 import "./Dashboard.css";
 
 function Dashboard() {
   return (
     <div className="dashboard">
       <h1>Emoji Critic — All About Emojis</h1>
-      <p>
+      <Link to="/reviews">
         The #1 Destination for Emoji Reviews on the World Wide Web Since 2020!
-      </p>
+      </Link>
     </div>
   );
 }
